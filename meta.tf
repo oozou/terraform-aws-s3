2c623f6ea711294c22658d756057f14cbfd1326c
waruwat <mai@oozou.com>
2022-03-07 14:16:15 +0700

    (format) terraform fmt

--- meta.tf
+++ meta.tf
@@ -3,7 +3,7 @@ data "aws_caller_identity" "main" {}
 data "aws_region" "active" {}
 
 locals {
-  prefix = "${var.prefix}-${var.environment}-${var.bucket_name}"
+  prefix         = "${var.prefix}-${var.environment}-${var.bucket_name}"
   bucket_name    = var.centralize_hub ? "${local.prefix}-${data.aws_caller_identity.main.account_id}-${random_string.random_suffix.result}" : "${local.prefix}-${random_string.random_suffix.result}"
   length_key_arn = length(keys(var.kms_key_arn))
   kms_key_arn    = local.length_key_arn != 0 ? values(var.kms_key_arn)[0] : module.bucket_kms_key[0].key_arn
