080d2343aefd75294d483ebe98565171bd389a18
artpasut <pasut67@gmail.com>
2022-06-02 14:37:48 +0700

    DTPK-122: fix kms_key_id for s3 object

--- folders.tf
+++ folders.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_object" "this" {
   bucket                 = aws_s3_bucket.this.bucket
   key                    = "${var.folder_names[count.index]}/.keep"
-  kms_key_id             = var.is_use_kms_managed_key ? local.kms_key_id : null
+  kms_key_id             = var.is_use_kms_managed_key ? local.kms_key_arn : null
   server_side_encryption = var.is_use_kms_managed_key ? "aws:kms" : "AES256"
 
   tags = merge({
