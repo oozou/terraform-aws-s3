250b63a73b89d77d54435f4fca34440e0a148871
sedthawut tipkanpirome <40098197+xshot9011@users.noreply.github.com>
2022-12-13 10:39:42 +0700

    (update): fix kms name

--- kms-key.tf
+++ kms-key.tf
@@ -5,8 +5,8 @@ module "bucket_kms_key" {
   count = var.is_use_kms_managed_key && local.length_key_arn == 0 ? 1 : 0
 
   prefix               = var.prefix
-  name                 = "${var.bucket_name}-s3-kms"
   environment          = var.environment
+  name                 = "${var.bucket_name}-s3"
   append_random_suffix = true
   description          = "S3 bucket encryption KMS key"
   key_type             = "service"
